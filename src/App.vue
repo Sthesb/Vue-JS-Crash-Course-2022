f1d29b71ee34a84ce39d001327285b91091ad82b
Sthembiso Mathebula <Sthembiso.Mathebula@gijima.com>
2022-04-02 13:31:18 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>{{ title }}</h2>
+  <Modal />
+  
+</template>
+
+<script>
+import Modal from './components/Modal/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data () {
+    return {
+      title: 'Learning VueJS',
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="backdrop">
+        <div class="backdrop_modal">
+            <p>Modal Content</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scope>
+.backdrop {
+    top: 0;
+    position: fixed;
+    background-color: rgba(0,0,0,0.7);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.backdrop_modal {
+    width: 400px;
+    height: 100px;
+    
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.backdrop_modal > p {
+    padding: 0px;
+    margin: 0px;
+}
+</style>
