e0e1495cd88bfbe17e82d7264aa075e8ba1d5f15
Sthesb <smathebula.sb@gmail.com>
2022-04-08 22:45:29 +0200

    App is complete

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <button class="btn btn-primary" @click="start" :disabled="isPlaying">Play</button>
     <Block v-if="isPlaying" :delay="delay" @end="endGame" />
     <h4 v-if="score !== null">Reaction time: {{ score }} ms</h4>
-    <Results v-if="score !== null" :result="result" />
+    <Results v-if="score !== null" :score="score" />
   
 </template>
 
@@ -34,13 +34,7 @@ export default {
       this.isPlaying = false;
       this.score = reactionTimer;
 
-      if(reactionTimer < 300){
-        this.result = "You're a G"
-      }else if ( reactionTimer >= 300  && reactionTimer < 500){
-        this.result = "You're slow. try agin my G"
-      }else {
-        this.result = "You're Mabhena"
-      }
+      
 
 
     }
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,9 +1,24 @@
 <template>
     <h4 class="mt-3">{{ result }}</h4>
+
 </template>
 
 <script>
 export default {
-    props: ['result']
+    props: ['score'],
+    data() {
+        return {
+            result: null
+        }
+    },
+    mounted(){
+        if(this.score < 300){
+            this.result = "You're a G"
+        }else if ( this.score >= 300  && this.score < 500){
+            this.result = "You're slow. try agin my G"
+        }else {
+            this.result = "You're Mabhena"
+        }
+    }
 }
 </script>
