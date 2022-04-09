daddf1a6d7ce7030946848b7ba8ef9769f8f0c7d
Sthesb <smathebula.sb@gmail.com>
2022-04-10 01:06:59 +0200

    Forms and inputs

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,21 @@
 <template>
-    
-    <h2>Ninja Reaction Timer</h2> <br>
-    <button class="btn btn-primary" @click="start" :disabled="isPlaying">Play</button>
-    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-    <h4 v-if="score !== null">Reaction time: {{ score }} ms</h4>
-    <Results v-if="score !== null" :score="score" />
-  
+    <p>working forms</p>
+   
 </template>
 
 <script>
-import Block from './components/Block.vue'
-import Results from './components/Results.vue'
+
 
 export default {
   name: 'App',
-  components: { Block, Results },
+  components: {  },
   data () {
     return {
-      isPlaying: false,
-      delay: null,
-      score: null,
-      result: ''
+      
     }
   },
   methods: {
-    start () {
-      this.score = null;
-      this.result = ''
-      this.delay = 2000 + Math.random() * 5000
-      this.isPlaying = true
-    },
-    endGame(reactionTimer){
-      this.isPlaying = false;
-      this.score = reactionTimer;
-
-      
-
-
-    }
+    
   }
   
 }
